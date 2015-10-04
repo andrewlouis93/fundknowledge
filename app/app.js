9d2bd1b2a170a3fceb52182658a30c3c482adbac
Andrew Louis <andrew.louis93@gmail.com>
2015-10-04 02:27:54 -0400

    Added views, db hooks

--- app/app.js
+++ app/app.js
@@ -5,18 +5,6 @@ var express = require('express')
 var routes = require('./routes');
 var app = module.exports = express.createServer();
 
-/* MongoDB Connection */
-var url = 'mongodb://localhost:27017/frontend';
-var MongoClient = require('mongodb').MongoClient;
-var assert = require('assert');
-
-// Use connect method to connect to the Server 
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected correctly to server");
-  db.close();
-});
-
 // Configuration
 app.configure(function(){
   app.set('views', __dirname + '/views');
@@ -27,17 +15,49 @@ app.configure(function(){
   app.use(express.static(__dirname + '/public'));
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+
+/* MongoDB Connection */
+var url = 'mongodb://localhost:1337/frontend';
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+
+// Use connect method to connect to the Server 
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server");
+
+  app.listen(3000, function(){
+    console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+  });  
+
+  // Mongo Collections
+  var People = db.collection('Person');
+  var NGOS = db.collection('NGO');
+
+  app.configure('development', function(){
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+  });
+
+  app.configure('production', function(){
+    app.use(express.errorHandler());
+  });
+
+  // Routes
+  app.get('/', routes.index);
+
+  // Routes to add a person
+  app.get('/add/p', routes.addPerson);
+  app.post('/add/p', routes.submitPerson);
+  // Display Person Information page
+  app.get('/p/:person_id', routes.person);
+
+  // Routes to add an NGO
+  app.get('/add/org', routes.addNGO);
+  app.post('/add/org', routes.submitNGO);
+  // Display NGO Information page
+  app.get('/n/:ngo_id', routes.NGO)
 });
 
-app.configure('production', function(){
-  app.use(express.errorHandler());
-});
 
-// Routes
-app.get('/', routes.index);
 
-app.listen(3000, function(){
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
-});
+
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,73 @@
-
 /*
  * GET home page.
  */
-
 exports.index = function(req, res){
   res.render('index', { title: 'Express' })
 };
+
+
+exports.addPerson = function(req, res){
+	res.render('addPerson');
+};
+exports.addNGO = function(req, res){
+	res.render('addNGO');
+}
+
+var url = 'mongodb://localhost:1337/frontend';
+var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectID;
+var assert = require('assert');
+
+MongoClient.connect(url, function(err, db) {
+	 
+	 var People = db.collection('Person');
+	 var NGOs = db.collection('NGO');
+
+	exports.person = function(req, res){
+		var person_id = req.params.person_id;
+		var _p = People.find({_id : ObjectId(person_id)}).toArray(function (err, data) {
+		  // data[0] is your document here
+		  console.log("in person")
+		  console.log( data[0] );
+		  res.render( 'person', { person: data[0] } );
+		})
+	};
+
+	exports.NGO = function(req, res){
+		var ngo_id = req.params.ngo_id;
+		var ngo = NGOs.find({_id : ObjectId(ngo_id)}).toArray(function (err, data) {
+		  // data[0] is your document here
+		  console.log("in ngo")
+		  console.log( data[0] );
+		  res.render( 'ngo', { ngo: data[0] } );
+		})
+	};
+
+	 exports.submitPerson = function(req, res){
+		var person = {
+			name: req.body.inputName, 
+			twitter: req.body.inputTwitter,
+			skype: req.body.inputSkype,
+			age: req.body.age, 
+			target: req.body.targetFunds,
+			raised: 0
+		}
+		People.insert(person);
+		// FLASH MESSAGE
+		res.redirect('/');
+	};
+  
+	 exports.submitNGO = function(req, res){
+		var NGO = {
+			name: req.body.inputName, 
+			email: req.body.inputEmail,
+			twitter: req.body.inputTwitter,
+		}
+		NGOs.insert(NGO);
+		// FLASH MESSAGE
+		res.redirect('/');
+	};
+
+});
+
+
