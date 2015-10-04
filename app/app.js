d8c9d4acc88ead71978b1985c95be1e954fcc2b7
Ubuntu <fk@fk2.fk2.g1.internal.cloudapp.net>
2015-10-04 06:13:45 +0000

    add in login

--- app/app.js
+++ app/app.js
@@ -2,8 +2,23 @@
  * Module dependencies.
  */
 var express = require('express')
+var config = require('./oauth.js')
+var passport = require('passport')
+var FacebookStrategy = require('passport-facebook').Strategy;
+var GoogleStrategy = require('passport-google').Strategy;
 var routes = require('./routes');
 var app = module.exports = express.createServer();
+
+/* Passport stuff */
+// serialize and deserialize
+passport.serializeUser(function(user, done) {
+done(null, user);
+});
+passport.deserializeUser(function(obj, done) {
+done(null, obj);
+});
+
+
 
 /* MongoDB Connection */
 var url = 'mongodb://localhost:27017/frontend';
@@ -21,8 +36,12 @@ MongoClient.connect(url, function(err, db) {
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'ejs');
+  app.use(express.cookieParser())
   app.use(express.bodyParser());
   app.use(express.methodOverride());
+  app.use(express.session( { secret: '8632248974' }))
+  app.use(passport.initialize());
+  app.use(passport.session())
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
@@ -35,9 +54,80 @@ app.configure('production', function(){
   app.use(express.errorHandler());
 });
 
+// serialize and deserialize
+passport.serializeUser(function(user, done) {
+ console.log('serializeUser: ' + user._id)
+ done(null, user._id);
+});
+passport.deserializeUser(function(id, done) {
+ User.findById(id, function(err, user){
+     console.log(user)
+     if(!err) done(null, user);
+     else done(err, null)
+ })
+});
+
+passport.use(new GoogleStrategy({
+ returnURL: config.google.returnURL,
+ realm: config.google.realm
+},
+function(identifier, profile, done) {
+ process.nextTick(function () {
+   profile.identifier = identifier;
+   return done(null, profile);
+ });
+}
+));
+
 // Routes
 app.get('/', routes.index);
+//Passport routes
+app.get('/ping', routes.ping);
+app.get('/account', ensureAuthenticated, function(req, res){
+User.findById(req.session.passport.user, function(err, user) {
+ if(err) {
+   console.log(err);
+ } else {
+   res.render('account', { user: user});
+ };
+});
+});
 
-app.listen(3000, function(){
+app.get('/', function(req, res){
+res.render('login', { user: req.user });
+});
+
+app.get('/auth/facebook',
+passport.authenticate('facebook'),
+function(req, res){
+});
+app.get('/auth/facebook/callback',
+passport.authenticate('facebook', { failureRedirect: '' }),
+function(req, res) {
+ res.redirect('/account');
+});
+
+app.get('/auth/google',
+passport.authenticate('google'),
+function(req, res){
+});
+app.get('/auth/google/callback',
+passport.authenticate('google', { failureRedirect: '/' }),
+function(req, res) {
+ res.redirect('/account');
+});
+
+app.get('/logout', function(req, res){
+req.logout();
+res.redirect('/');
+});
+
+
+app.listen(1337, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
+
+function ensureAuthenticated(req, res, next) {
+if (req.isAuthenticated()) { return next(); }
+res.redirect('/')
+}
